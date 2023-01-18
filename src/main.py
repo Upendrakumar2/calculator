ac22f4cac48fdc7ca1ae0438467b0a3ca98de794
Upendrakumar2 <upendra.kumar@tigeranalytics.com>
2023-01-18 18:50:46 +0530

    canged py version

--- src/main.py
+++ src/main.py
@@ -3,12 +3,11 @@ import mlflow
 import calculator.logic
 import calculator.logger
 
-
+num1=5
+num2=6
 data_path="datasets"
 
 with mlflow.start_run(run_name="calculator",) as run:
-    num1=int(input("input the number one: "))
-    num2=int(input("input the number one: "))
     ans=calculator.logic.add(num1,num2)
     print("add is being done.")
     mlflow.log_param(key="first", value=num1)
